08453ddd1e3f7c7ab387147912f6b33a652e5cbb
Oleksandr Husiev <ohusiev@icloud.com>
2024-10-11 21:17:24 +0200

    added base parts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use num::Complex;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn complex_square_add_loop(c: Complex<f64>) {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    loop {
+        z = z * z + c;
+    }
+}
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+    None
 }
